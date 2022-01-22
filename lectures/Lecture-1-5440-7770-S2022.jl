c655947d6cb724b21af141c33a3c056c3531c96a
Varnerlab <jdv27@cornell.edu>
2022-01-22 17:11:49 -0500

    Lecture 1
    lecture 1 notebook

---
+++ lectures/Lecture-1-5440-7770-S2022.jl
@@ -0,0 +1,30 @@
+### A Pluto.jl notebook ###
+# v0.17.7
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 45e60528-7b8d-11ec-3aa3-65625ebbbfb2
+md"""
+## L1: Introduction to Metabolism and Metabolic Networks
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.1"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─45e60528-7b8d-11ec-3aa3-65625ebbbfb2
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002

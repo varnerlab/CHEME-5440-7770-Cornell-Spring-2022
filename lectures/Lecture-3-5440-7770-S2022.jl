5195d39bd54224fd52f7b8e71bb9e4c98dd1a63c
Varnerlab <jdv27@cornell.edu>
2022-01-30 17:29:32 -0500

    Update Lecture-3-5440-7770-S2022.jl
    still messed up

--- lectures/Lecture-3-5440-7770-S2022.jl
+++ lectures/Lecture-3-5440-7770-S2022.jl
@@ -7,6 +7,19 @@ using InteractiveUtils
 # ╔═╡ 9e2566fd-4fc9-4828-913f-f755c5e5cf50
 idx_ep = 4
 
+# ╔═╡ b14d750b-5f36-46c7-bdb8-7deba7ac0bb7
+function is_convex_independent(rᵢ,rⱼ)::Bool
+	idx_zero_rᵢ = findall(x->x==0.0,rᵢ)
+	idx_zero_rⱼ = findall(x->x==0.0,rⱼ)	
+	return issubset(idx_zero_rᵢ,idx_zero_rⱼ) ? false : true
+end
+
+# ╔═╡ 2bd35555-b771-43b8-acec-86c8d7d20fcd
+function is_cycle(rᵢ::Array{Float64,1})::Bool
+	idx_zero_rᵢ = findall(x->x!=0.0,rᵢ)
+	return length(idx_zero_rᵢ) == 2 ? true : false
+end
+
 # ╔═╡ 907af081-4a01-4a51-bda9-3a34295f9208
 function ingredients(path::String)
 	
@@ -29,6 +42,9 @@ begin
 	# import some packages -
 	using PlutoUI
 	using LinearAlgebra
+	using RowEchelon
+	using IterativeSolvers
+	using Combinatorics
 	
 	# setup paths -
 	const _PATH_TO_NOTEBOOK = pwd()
@@ -53,28 +69,20 @@ begin
 	reaction_array = Array{String,1}()
 
 	# encode the reactions -
-	push!(reaction_array,"v₁,A+ATP,B+ADP,false")
-	push!(reaction_array,"v₂,B+NAD,C+NADH,false")
-	push!(reaction_array,"v₃,C+ADP,D+ATP,false")
-	push!(reaction_array,"v₄,D+NADH,E+NAD,false")
-	push!(reaction_array,"v₅,D+NAD,F+NADH,false")
-	push!(reaction_array,"v₆,F+NADH,G+NAD,false") # change
-	push!(reaction_array,"v₇,H,F,false") # change
-	push!(reaction_array,"v₈,G+ATP,I+ADP,false") # change
-	push!(reaction_array,"v₉,I+NADH,H+NAD,false") # change
-	push!(reaction_array,"v₁₀,G,J,false")
-	push!(reaction_array,"vₐ,∅,A,false")
-	push!(reaction_array,"vₑ,E,∅,false")
-	push!(reaction_array,"vₕ,H,∅,false") # change
-	push!(reaction_array,"vⱼ,J,∅,false") # change
-	push!(reaction_array,"vATP,ATP,∅,false")
-	push!(reaction_array,"vADP,ADP,∅,false")
-	push!(reaction_array,"vNADH,NADH,∅,false")
-	push!(reaction_array,"vNAD,NAD,∅,false")
+	push!(reaction_array,"v₁,A,B,false")
+	push!(reaction_array,"v₂,B,C,false")
+	push!(reaction_array,"v₃,C,D,false")
+	push!(reaction_array,"v₄,C,E,false")
+
+	
+	push!(reaction_array,"b₁,∅,A,false")
+	push!(reaction_array,"b₂,∅,B,false")
+	push!(reaction_array,"b₃,D,∅,false")
+	push!(reaction_array,"b₄,E,∅,false")
 	
 	# compute the stoichiometric matrix -
 	(S, species_array, reaction_name_array) = lib.build_stoichiometric_matrix(reaction_array; 
-		expand=true);
+		expand=false);
 
 	# show -
 	nothing
@@ -83,19 +91,16 @@ end
 # ╔═╡ 8965f69d-3014-46b3-816d-7d6f7fd57adf
 (ℳ,ℛ) = size(S)
 
-# ╔═╡ f305c659-76d6-4a89-b231-80f5fbf1517a
-S
+# ╔═╡ faa86b5e-8e38-4efd-a59d-4a35a5b707fc
+species_array
 
 # ╔═╡ 276a5591-bf84-4750-95ea-9fc48ea7bacb
 function expa(stoichiometric_matrix::Array{Float64,2})
 
     # initialize -
-    (ℳ,ℛ) = size(stoichiometric_matrix) 
+	(ℳ,ℛ) = size(stoichiometric_matrix)
     T = [Matrix{Float64}(I,ℛ,ℛ) transpose(stoichiometric_matrix)];
-
-    # what is the dimension of the tables?
-    (NRT,NCT) = size(T)
-
+	
     # outer loop -
     for col_index = 1:ℳ
 
@@ -135,71 +140,82 @@ function expa(stoichiometric_matrix::Array{Float64,2})
     
 		# ok, so we have a new set of possible pathways, check for independent pathways -
         P = transpose(hcat(TMP_ARR...))
-        (NRP,_) = size(P)
-        OK_ARR = Matrix{Float64}(I,NRP,NRP);
+		rows_to_keep_set = Set{Int64}()
+		
+		(NRP,_) = size(P)
+		OK_ARR = Matrix{Float64}(I,NRP,NRP);
         for outer_row_index ∈ 1:NRP
-            for inner_row_index ∈ 1:NRP
-                
-                if (outer_row_index != inner_row_index)
-                    
-                    # grab the index of zeros for the outer and inner row -
-                    idx_zero_outer = findall(x->x==0.0,P[outer_row_index,:])
-                    idx_zero_inner = findall(x->x==0.0,P[inner_row_index,:])
 
-                    # Ok, so now what?
-				    Z = setdiff(idx_zero_inner,idx_zero_outer);
-				    if (isempty(Z))
-					    OK_ARR[outer_row_index, inner_row_index] = 0.0;
-					else
-						OK_ARR[outer_row_index, inner_row_index] = 1.0;				
+			# get rᵢ -
+			rᵢ = P[outer_row_index,:]
+
+			if (is_cycle(rᵢ) == false)
+				# assume we can keep (for now ..)
+				push!(rows_to_keep_set, outer_row_index)
+				
+				for inner_row_index ∈ 1:NRP
+					if (outer_row_index != inner_row_index)
+	
+						# get rⱼ -
+						rⱼ = P[inner_row_index,:]
+						
+						# check: is convex independent?
+						if (is_convex_independent(rᵢ,rⱼ) == true)
+							push!(rows_to_keep_set, outer_row_index)
+						else
+	
+							# oops: this rᵢ is NOT independent - if we have
+							# in the rows to keep set, we need to remove it
+							delete!(rows_to_keep_set,outer_row_index)
+						end
 					end
-                end
-
-            end # inner 
+				end # inner 
+			end # cycle
         end # outer
 
-
-		idx_rows_to_keep = Array{Int64,1}()
-		for row_index ∈ 1:NRP
-			TMP_OK_ROW = OK_ARR[row_index,:]
-			N = length(findall(x->x==1.0,TMP_OK_ROW))
-			if (N == NRP)
-				push!(idx_rows_to_keep,row_index)
-			end
-		end
-
-		# which rows should we keep?
-		P_IND = P[idx_rows_to_keep,:]
-
+		# grab: which rows should we keep?
+        P_IND = P[(rows_to_keep_set |> collect),:]
 		if (isempty(P_IND) == false)
 			# Update T -
 			T = vcat(TNEW,P_IND);
 		end
     end # main -
 
+	# check: remove cycles -
+	P = T[:,1:ℛ]
+	(NCP,_) = size(P)
+	is_not_cycle_set = Set{Int64}()
+	for i ∈ 1:NCP
+		if (is_cycle(P[i,:]) == false)
+			push!(is_not_cycle_set,i)
+		end
+	end
 	
     # return - 
-    return T
+    return T[(is_not_cycle_set |> collect), :]
 
 end # function -
 
 # ╔═╡ 8be1e489-7382-4315-8c4c-111abdead290
 PM = expa(S)
 
-# ╔═╡ c425a427-071c-45dc-a417-27a96f3da32e
-N = PM[:,1:ℛ]
+# ╔═╡ 46cdb1ff-84fe-4222-97aa-2a52369fdbf6
+P = PM[:,1:ℛ]
 
-# ╔═╡ 95bb3270-8487-43ee-9795-766dd36ad092
-N[3,:]
+# ╔═╡ 4054b145-9aee-49ed-b015-66b3370a2d38
+S*P[4,:]
 
 # ╔═╡ b25cf5a8-483a-418a-960c-ae6a9deedf8b
-idxnz = findall(x->x!=0,N[idx_ep,:])
+idxnz = findall(x->x!=0.0,P[idx_ep,:])
 
 # ╔═╡ 57b93c34-6528-41d9-8af3-b5b7fce5d9b6
 reaction_name_array[idxnz]
 
-# ╔═╡ bbfeb47e-8bbd-4c4b-9d29-65cb99ca26c1
-N[3,idxnz]
+# ╔═╡ 11094ebd-cd4b-4dfb-8810-afcf9f78ad26
+rank(P)
+
+# ╔═╡ 65163c1b-2db9-443c-96c1-e218966a5397
+rank(P)
 
 # ╔═╡ d201abce-202c-44f6-98a3-67e47c2a99f4
 html"""
@@ -270,11 +286,17 @@ html"""
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
+Combinatorics = "861a8166-3701-5b0c-9a16-15d98fcdc6aa"
+IterativeSolvers = "42fd0dbc-a981-5370-80f2-aaf504508153"
 LinearAlgebra = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
 PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+RowEchelon = "af85af4c-bcd5-5d23-b03a-a909639aa875"
 
 [compat]
+Combinatorics = "~1.0.2"
+IterativeSolvers = "~0.9.2"
 PlutoUI = "~0.7.32"
+RowEchelon = "~0.2.1"
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000002
@@ -304,6 +326,11 @@ deps = ["FixedPointNumbers", "Random"]
 git-tree-sha1 = "024fe24d83e4a5bf5fc80501a314ce0d1aa35597"
 uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
 version = "0.11.0"
+
+[[deps.Combinatorics]]
+git-tree-sha1 = "08c8b6831dc00bfea825826be0bc8336fc369860"
+uuid = "861a8166-3701-5b0c-9a16-15d98fcdc6aa"
+version = "1.0.2"
 
 [[deps.CompilerSupportLibraries_jll]]
 deps = ["Artifacts", "Libdl"]
@@ -343,6 +370,12 @@ version = "0.2.2"
 [[deps.InteractiveUtils]]
 deps = ["Markdown"]
 uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[deps.IterativeSolvers]]
+deps = ["LinearAlgebra", "Printf", "Random", "RecipesBase", "SparseArrays"]
+git-tree-sha1 = "1169632f425f79429f245113b775a0e3d121457c"
+uuid = "42fd0dbc-a981-5370-80f2-aaf504508153"
+version = "0.9.2"
 
 [[deps.JSON]]
 deps = ["Dates", "Mmap", "Parsers", "Unicode"]
@@ -425,10 +458,21 @@ uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
 deps = ["SHA", "Serialization"]
 uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
 
+[[deps.RecipesBase]]
+git-tree-sha1 = "6bf3f380ff52ce0832ddd3a2a7b9538ed1bcca7d"
+uuid = "3cdcf5f2-1ef4-517c-9805-6587b60abb01"
+version = "1.2.1"
+
 [[deps.Reexport]]
 git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
 uuid = "189a3867-3050-52da-a836-e630ba90ab69"
 version = "1.2.2"
+
+[[deps.RowEchelon]]
+deps = ["LinearAlgebra"]
+git-tree-sha1 = "f479526c4f6efcbf01e7a8f4223d62cfe801c974"
+uuid = "af85af4c-bcd5-5d23-b03a-a909639aa875"
+version = "0.2.1"
 
 [[deps.SHA]]
 uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
@@ -486,15 +530,18 @@ uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
 # ╔═╡ Cell order:
 # ╠═be4b1854-1a14-4452-ad7e-22d614740a10
 # ╠═8965f69d-3014-46b3-816d-7d6f7fd57adf
-# ╠═f305c659-76d6-4a89-b231-80f5fbf1517a
 # ╠═8be1e489-7382-4315-8c4c-111abdead290
-# ╠═c425a427-071c-45dc-a417-27a96f3da32e
+# ╠═46cdb1ff-84fe-4222-97aa-2a52369fdbf6
+# ╠═4054b145-9aee-49ed-b015-66b3370a2d38
 # ╠═9e2566fd-4fc9-4828-913f-f755c5e5cf50
-# ╠═95bb3270-8487-43ee-9795-766dd36ad092
 # ╠═b25cf5a8-483a-418a-960c-ae6a9deedf8b
-# ╠═bbfeb47e-8bbd-4c4b-9d29-65cb99ca26c1
 # ╠═57b93c34-6528-41d9-8af3-b5b7fce5d9b6
+# ╠═11094ebd-cd4b-4dfb-8810-afcf9f78ad26
+# ╠═faa86b5e-8e38-4efd-a59d-4a35a5b707fc
+# ╠═65163c1b-2db9-443c-96c1-e218966a5397
 # ╠═276a5591-bf84-4750-95ea-9fc48ea7bacb
+# ╠═b14d750b-5f36-46c7-bdb8-7deba7ac0bb7
+# ╠═2bd35555-b771-43b8-acec-86c8d7d20fcd
 # ╠═c51f8430-8147-11ec-3501-498c9bf67b57
 # ╠═907af081-4a01-4a51-bda9-3a34295f9208
 # ╠═d201abce-202c-44f6-98a3-67e47c2a99f4
